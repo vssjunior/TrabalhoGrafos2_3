727d8b24afba66c43388fdf7263162fcb9309a15
Raydson Ferreira carlota <raydsonferreira@gmail.com>
2017-08-11 22:21:54 -0300

    leitura concluida e inicializando junção de sub-Regiões

--- TP2_3/beer.cpp
+++ TP2_3/beer.cpp
@@ -2,8 +2,29 @@
 #include <fstream>
 #include "grafos.cpp"
 #include "objetos.cpp"
+#include <list>
 
 using namespace std;
+
+void montarSubRegiao(Grafo* grafo, DataVertice* vertices, InfoSubReg infoSubReg, list<DataVertice> **subReg, double mediaConsumidor, double mediaDemanda, double mediaCargaTrabalho){
+	double mediaInferiorConsumidores = mediaConsumidor - infoSubReg.lambdaConsumidores;
+	double mediaSuperiorConsumidores = mediaConsumidor + infoSubReg.lambdaConsumidores;
+	double mediaInferiorDemanda = mediaDemanda - infoSubReg.lambdaDemanda;
+	double mediaSuperiorDemanda = mediaDemanda + infoSubReg.lambdaDemanda;
+	double mediaInferiorCargaTrabalho = mediaCargaTrabalho - infoSubReg.lambdaCargaTrabalho;
+	double mediaSuperiorCargaTrabalho = mediaCargaTrabalho + infoSubReg.lambdaCargaTrabalho;
+	
+	for(int i = 0; i < grafo -> getQuantVertices(); i++){
+		if(vertices[i].consumidores - mediaInferiorConsumidores and vertices[i].consumidores < mediaSuperiorConsumidores
+			and vertices[i].demanda > mediaInferiorDemanda) {
+			
+		}
+	}
+	
+	
+	
+	
+}
 
 int main(){
 	Grafo* grafo;
@@ -19,9 +40,8 @@ int main(){
 	
 	DataVertice* vertices = new DataVertice[tam];
 	
-	int aux;
 	for(int i=0; i<tam; i++){
-		arq >> aux;
+		arq >> vertices[i].id;
 		arq >> vertices[i].x;
 		arq >> vertices[i].y;
 		arq >> vertices[i].consumidores;
@@ -55,11 +75,30 @@ int main(){
 	double somaConsu = 0;
 	double somaDeman = 0;
 	double somaCTrab = 0;
+	
 	for(int i = 0; i < tam; i++){
 		somaConsu += vertices[i].consumidores;
 		somaDeman += vertices[i].demanda;
 		somaCTrab += vertices[i].cargaTrabalho;
 	}
 	
+	infoSubReg.betaConsumidores = somaConsu/tam;
+	infoSubReg.betaDemanda = somaDeman/tam;
+	infoSubReg.betaCargaTrabalho = somaCTrab/tam;
+	
+	double auxConsumidores, auxDemanda, auxCargaTrabalho;
+	
+	for(int i=0; i<tam; i++){
+		auxConsumidores = abs(vertices[i].consumidores
+	cout << infoSubReg.betaConsumidores << "consumidor\n";
+	cout << infoSubReg.betaDemanda << "demanda\n";
+	cout << infoSubReg.betaCargaTrabalho << "carga\n";
+	
+	list<DataVertice> **subReg = new list<DataVertice>*[infoSubReg.qntReg];	
+	for(int i=0; i<infoSubReg.qntReg; i++){
+		subReg[i] = new list<DataVertice>;
+	}
+	
+	montarSubRegiao(grafo, vertices, infoSubReg, subReg, infoSubReg.betaConsumidores, infoSubReg.betaDemanda, infoSubReg.betaCargaTrabalho);
 	return 0;
 }
